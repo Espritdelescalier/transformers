35eedfb0d47d809ae7e18122c3e4b302f53a4f83
Damien Francois <damien@trux-dgx01.uni.lux>
2023-08-10 16:25:33 +0200

    curbert and attention stuff

--- confirm_generation.py
+++ confirm_generation.py
@@ -10,10 +10,14 @@ model = CURForCausalLM.from_pretrained(
     "./checkpoints_wiki/").to(torch_device)
 
 
-model_inputs = tokenizer('As a note, the particular relationship of our model to the model of Krotov & Hopfield (2020), is what they refer to as a ‘type B’ model. These are models with contrastive normalisation on the memory neurons (via a softmax in our case), as opposed to ‘type B’ models which have a power activation function on the memory neurons. TEM (left hand side of Equation 11) corresponds to a linear activation.', return_tensors='pt').to(torch_device)
+model_inputs = tokenizer(
+    'My cat is sick so I will take him to', return_tensors='pt').to(torch_device)
 
 # generate 40 new tokens
-greedy_output = model.generate(**model_inputs, max_new_tokens=40)
+greedy_output = model.generate(**model_inputs,
+                               max_new_tokens=40,
+                               no_repeat_ngram_size=2,
+                               early_stopping=True)
 
 print("Output:\n" + 100 * '-')
 print(tokenizer.decode(greedy_output[0], skip_special_tokens=True))
